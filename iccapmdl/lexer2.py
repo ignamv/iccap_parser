9de9b415caefafd3ef5af682eefb90b8d8acba67
Ignacio Martinez Vazquez <ignamv@gmail.com>
2017-01-01 16:32:08 -0300

    Many changes
    Pylint compliance
    Add __all__
    Translate LINK types to corresponding IC-CAP mytype
    Refactor parsefile if tree
    Remove empty DATA nodes

--- iccapmdl/lexer2.py
+++ iccapmdl/lexer2.py
@@ -1,71 +1,79 @@
 '''IC-CAP .mdl file parsing routines'''
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 import re
-import numpy as np
-import sys
 from os import SEEK_CUR
 import logging
+import numpy as np
 logger = logging.getLogger(__name__)
 
+__all__ = [
+    'parsefile'
+]
+
 WORD = re.compile(rb'[^" ]+|"[^"]*"|^\s*$')
+LBRACE = (b'{\r\n',)
+RBRACE = (b'}\r\n',)
+LINK_TYPES = {
+    'MODEL': 'Model',
+    'DUT': 'Dut',
+    'DAT': 'Setup',
+    'SWEEP': 'Input',
+    'OUT': 'Output'
+}
 
 
-def lextoken(line):
-    '''Create token (list of words) from line'''
-    return WORD.findall(line)
-
-
-def skippoints(fd, npoints):
+def skippoints(stream, npoints):
     '''Skip a dataset'''
     position = 0
     while npoints - position > 3:
         # Each point line is at least 16 bytes
-        fd.seek((npoints - position - 1) * 16, SEEK_CUR)
+        stream.seek((npoints - position - 1) * 16, SEEK_CUR)
         # Discard partial line
-        fd.readline()
+        stream.readline()
         # Find current poisition
-        words = WORD.findall(fd.readline())
+        words = WORD.findall(stream.readline())
         if not words[0] == b'point':
             raise Exception(words)
         position = int(words[1]) + 1
     for position in range(position, npoints-1):
-        fd.readline()
-    line = fd.readline()
+        stream.readline()
+    line = stream.readline()
     if int(WORD.findall(line)[1]) != npoints - 1:
         raise Exception(line)
 
 
-def lexfile(fd):
-    for line in fd:
-        tok = lextoken(line)
-        if tok[0] == b'datasize':
-            npoints = int(tok[2])
-            fd.readline()
-            skippoints(fd, npoints)
-            if tok[1] == 'BOTH':
-                fd.readline()
-                skippoints(fd, npoints)
-
-
-def readtoken(fd):
-    line = fd.readline()
+def readtoken(stream):
+    '''Read a token (list of words in a line) from file'''
+    line = stream.readline()
     if not line:
-        raise Exception()
-    return WORD.findall(line)
+        raise StopIteration()
+    return tuple(WORD.findall(line))
+
+
+def tokens(stream):
+    '''Yield tokens from file'''
+    while True:
+        try:
+            yield readtoken(stream)
+        except StopIteration:
+            return
 
 
 class Node(namedtuple('Node_', ['type_', 'name', 'children', 'vars'])):
+    '''Element of .mdl file tree'''
     def __repr__(self, indent=0):
-        children = ',\n'.join(c.__repr__(indent + 1) for c in self.children)
+        children = ','.join('\n' + c.__repr__(indent + 1)
+                            for c in self.children)
         space = ' ' * indent
-        return '{space}Node({!r}, {!r}, [\n{children}{space}], {!r})'.format(
+        return '{space}Node({!r}, {!r}, [{children}{space}], {!r})'.format(
             self.type_, self.name, self.vars, children=children, space=space)
 
 
-def skipblock(fd):
+def skipblock(stream):
+    '''Skip lines until brace-delimited block is over'''
     level = 1
     while level > 0:
-        line = fd.readline()
+        line = stream.readline()
         if line.startswith(b'{'):
             level += 1
         elif line.startswith(b'}'):
@@ -75,86 +83,124 @@ def skipblock(fd):
         logger.debug('Skipped line %s', line)
 
 
-def read_points(fd, npoints):
-    real, imag = np.genfromtxt(fd, delimiter=' ', max_rows=npoints,
-                               usecols=(4, 5), unpack=True)
-    if (imag == 0).all():
-        return real
+def read_points(stream, npoints):
+    '''Read dataset as numpy array'''
+    value = np.genfromtxt(stream, delimiter=' ', max_rows=npoints,
+                          usecols=(4, 5), unpack=True)
+    if (value[:, 1] == 0).all():
+        return value[:, 0]
     else:
-        return real + 1j * imag
+        return value.view(complex)
 
 
-def parsefile(fd):
+def parsefile(stream):
+    '''Read Node tree from file'''
     path = [Node(b'Main', b'', [], {})]
-    lbrace = [b'{\r\n']
-    rbrace = [b'}\r\n']
+    handlers = OrderedDict()
+
+    def handle(*prefix):
+        '''Register function as handler for tokens starting with prefix'''
+        def wrap(func):
+            '''-'''
+            handlers[prefix] = func
+            return func
+        return wrap
+
+    @handle(RBRACE[0])
+    def _rbrace():
+        '''Add current node to children of the previous node in path'''
+        previous = path.pop()
+        parent = path[-1]
+        if previous.type_ == b'':
+            # Collapse contents of empty Node (from DATA token) to parent
+            parent.children.extend(previous.children)
+            parent.vars.update(previous.vars)
+        else:
+            parent.children.append(previous)
+
+    @handle(b'LINK')
+    def _link(type_, name, *dummy):
+        '''Add empty node to path'''
+        name = name[1:-1]
+        type_ = LINK_TYPES.get(type_, type_)
+        path.append(Node(type_, name, [], {}))
+        assert readtoken(stream) == LBRACE
+
+    @handle(b'data\r\n')
+    def _data():
+        '''Translate data to empty Node'''
+        path.append(Node(b'', b'', [], {}))
+        assert readtoken(stream) == LBRACE
+
+    @handle(b'TABLE', b'"Variable Table"')
+    def _dut_model_vars(*dummy):
+        '''Read dut/model vars into last Node in path'''
+        assert readtoken(stream) == LBRACE
+        while True:
+            token = readtoken(stream)
+            assert token[0] == b'element'
+            name = token[3][1:-1]
+            token = readtoken(stream)
+            assert token[0] == b'element'
+            value = token[3][1:-1]
+            if not name and not value:
+                assert readtoken(stream) == RBRACE
+                break
+            path[-1].vars[name] = value
+
+    @handle(b'HYPTABLE')
+    def _tablevars(*dummy):
+        '''Read TableVars into last Node in path'''
+        assert readtoken(stream) == LBRACE
+        while True:
+            token = readtoken(stream)
+            if token == RBRACE:
+                break
+            assert token[0] == b'element'
+            path[-1].vars[token[1][1:-1]] = token[2][1:-1]
+
+    @handle(b'dataset\r\n')
+    def _dataset():
+        '''Read dataset into vars MEAS or SIMU of last node in path'''
+        assert readtoken(stream) == LBRACE
+        token = readtoken(stream)
+        assert token[0] == b'datasize'
+        npoints = int(token[2])
+        while True:
+            token = readtoken(stream)
+            if token[0].startswith(b'}'):
+                break
+            assert token[0] == b'type'
+            dset = read_points(stream, npoints)
+            path[-1].vars[token[1]] = dset
+            logger.debug('Dataset at %s shape %s dtype %s', stream.tell(),
+                         dset.shape, dset.dtype)
+
+    @handle(b'point')
+    def _point(*token):
+        '''Should have been skipped by dataset handler, raise exception'''
+        raise Exception(*token)
+
+    @handle(LBRACE[0])
+    def _lbrace():
+        '''This wasn't read by other handlers so skip the block'''
+        logger.debug('Skip from %s at %s', token[0], stream.tell())
+        skipblock(stream)
+        logger.debug('Skip to %s', stream.tell())
+
     while True:
         try:
-            tok = readtoken(fd)
-        except:
+            token = readtoken(stream)
+        except StopIteration:
             return path[0]
-        if tok[0].startswith(b'}'):
-            previous = path.pop()
-            path[-1].children.append(previous)
-            continue
-        elif tok[0] == b'LINK':
-            type_ = tok[1]
-            name = tok[2][1:-1]
-            path.append(Node(type_, name, [], {}))
-            assert readtoken(fd) == lbrace
-        elif tok[0] == b'data\r\n':
-            path.append(Node(b'', b'', [], {}))
-            assert readtoken(fd) == lbrace
-        elif tok[0] == b'TABLE' and tok[1] == b'"Variable Table"':
-            assert readtoken(fd) == lbrace
-            while True:
-                tok = readtoken(fd)
-                assert tok[0] == b'element'
-                name = tok[3][1:-1]
-                tok = readtoken(fd)
-                assert tok[0] == b'element'
-                value = tok[3][1:-1]
-                if not name and not value:
-                    assert readtoken(fd) == rbrace
-                    break
-                path[-1].vars[name] = value
-        elif tok[0] == b'HYPTABLE':
-            assert readtoken(fd) == lbrace
-            while True:
-                tok = readtoken(fd)
-                if tok == rbrace:
-                    break
-                assert tok[0] == b'element'
-                path[-1].vars[tok[1][1:-1]] = tok[2][1:-1]
-        elif tok[0] == b'dataset\r\n':
-            assert readtoken(fd) == lbrace
-            tok = readtoken(fd)
-            assert tok[0] == b'datasize'
-            npoints = int(tok[2])
-            while True:
-                tok = readtoken(fd)
-                if tok[0].startswith(b'}'):
-                    break
-                assert tok[0] == b'type'
-                dset = read_points(fd, npoints)
-                path[-1].vars[tok[1]] = dset
-                logger.debug('Dataset at %s shape %s dtype %s', fd.tell(),
-                             dset.shape, dset.dtype)
-        elif tok[0] == b'point':
-            raise Exception(tok)
-        elif tok[0].startswith(b'{'):
-            logger.debug('Skip from %s at %s', tok[0], fd.tell())
-            skipblock(fd)
-            logger.debug('Skip to %s', fd.tell())
+        for prefix, handler in handlers.items():
+            if token[:len(prefix)] == prefix:
+                handler(*token[len(prefix):])
+                break
         else:
-            logger.debug('Skip token %s', tok[0])
+            logger.debug('Skip token %s', token[0])
 
 
-filename = '/home/ignamv/programacion/iccap/examples/model_files/mosfet/'\
-           'bsim4/examples/bsim4_binned/bsim4_binned~data.mdl'
-for ii in range(10):
-    with open(filename, 'rb') as fd:
-        lexfile(fd)
 #      1 View
 #     37 CNTABLE
 #     38 circuitdeck
