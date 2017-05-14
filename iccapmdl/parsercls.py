306fa2378ea39a928c0ce9165c454938450799a4
Ignacio Martinez Vazquez <ignamv@gmail.com>
2017-05-14 14:37:09 -0300

    Delete Ply-based parser

--- iccapmdl/parsercls.py
+++
@@ -1,7 +0,0 @@
-from ply.yacc import yacc
-
-class Parser(object):
-    def build(self, **kwargs):
-        self.parser = yacc(module=self, **kwargs)
-    def parse(self, *args, **kwargs):
-        return self.parser.parse(*args, **kwargs)
